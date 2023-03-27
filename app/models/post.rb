c2bfc745622369e0c6bed28258b63c02d7d1d1a2
eyal gerby <eyalg535@gmail.com>
2023-03-27 11:42:35 -0400

    commit tree 3c341148afb53dacd64cb859a14ec6c1cd8d9976

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+  validate :title_sufficient
+
+  def title_sufficient
+    valid = ["Won't Believe", "Secret", "Top", "Guess"].any? { |phrase| title.to_s.include?  (phrase)  }
+    unless valid
+      errors.add(:title, "Title must be more click-baity!")
+    end
+  end
 end
